6ceae0bd0f0e137e9158dfd119afc4bdfc107157
Hannu Väisänen <Hannu.Vaisanen@uef.fi>
2017-02-06 08:21:57 +0200

    Päivitys Solr:n versioon 6.4.0.

--- src/main/resources/install-sudo.sh
+++ src/main/resources/install-sudo.sh
@@ -13,5 +13,5 @@ sudo -u solr mkdir -p /var/solr/data/sukija/conf /var/solr/data/sukija/lib
 sudo -u solr cp -r conf/* /var/solr/data/sukija/conf/
 sudo -u solr cp $HOME/.m2/repository/net/java/dev/jna/jna/4.2.2/jna-4.2.2.jar /var/solr/data/sukija/lib/
 sudo -u solr cp $HOME/.m2/repository/org/puimula/voikko/libvoikko/4.0.1/libvoikko-4.0.1.jar /var/solr/data/sukija/lib/
-sudo -u solr cp $HOME/.m2/repository/peltomaa/sukija/sukija/2.1.6/sukija-2.1.6.jar /var/solr/data/sukija/lib/
+sudo -u solr cp $HOME/.m2/repository/peltomaa/sukija/sukija/2.1.7/sukija-2.1.7.jar /var/solr/data/sukija/lib/
 sudo -u solr cp $HOME/.m2/repository/org/ahocorasick/ahocorasick/0.3.0/ahocorasick-0.3.0.jar /var/solr/data/sukija/lib/
